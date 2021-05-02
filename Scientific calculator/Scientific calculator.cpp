87f1f6bbecf68badc106156cca4d5dccac28cbc2
Robby Sodhi <42954808+Robstersgaming@users.noreply.github.com>
2021-05-02 17:09:03 -0400

    single integer

--- Scientific calculator/Scientific calculator.cpp
+++ Scientific calculator/Scientific calculator.cpp
@@ -1,20 +1,122 @@
-// Scientific calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <sstream>
 
-int main()
+using namespace std;
+
+bool isOperator(char input)
 {
-    std::cout << "Hello World!\n";
+    switch (input) {
+    case '+':
+    case '-':
+    case '*':
+    case '%':
+    case '/':
+    case '^':
+        return true;
+    default:
+        return false;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int Prec(char input)
+{
+    switch (input) {
+    case '+':
+    case '-':
+        return 2;
+    case '*':
+    case '%':
+    case '/':
+        return 3;
+    case '^':
+        return 6;
+    case '(':
+        return 1;
+    }
+}
+bool isOperand(char input)
+{
+    if (isalpha(input) || isdigit(input))
+        return true;
+    return false;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string infixToPostfix(string input) {
+    stack<char> opstack;
+    string output;
+
+    for (int i{ 0 }; i < input.size(); i++) {
+        if (isOperand(input[i])) {
+            output += input[i];
+        }
+        else if (input[i] == '(') {
+            opstack.push(input[i]);
+        }
+        else if (input[i] == ')') {
+            char popped{ opstack.top() };
+            opstack.pop();
+            while (popped != '(') {
+                output += popped;
+                popped = opstack.top();
+                opstack.pop();
+            }
+        }
+        else if (isOperator(input[i])) {
+            while (!opstack.empty() && Prec(opstack.top()) >= Prec(input[i])) {
+                output += opstack.top();
+                opstack.pop();
+            }
+            opstack.push(input[i]);
+        }
+    }
+
+    while (!opstack.empty()) {
+        output += opstack.top();
+        opstack.pop();
+    }
+
+    return output;
+}
+double performMathOperation(char opp, double first, double second) {
+    switch (opp) {
+    case '+' :
+        return first + second;
+    case '-':
+        return first - second;
+    case '*':
+        return first * second;
+    case '/':
+        return first / second;
+    }
+}
+double evaluteExpression(string expression) {
+    string postfix = infixToPostfix(expression);
+
+    stack<double> opperands;
+    for (int i = 0; i < postfix.size(); i++) {
+        if (isdigit(postfix[i])) {
+            opperands.push(stod(string(1, postfix[i])));
+        }
+        else if (isOperator(postfix[i])) {
+            double second = opperands.top();
+            opperands.pop();
+            double first = opperands.top();
+            opperands.pop();
+            double result = performMathOperation(postfix[i], first, second);
+            opperands.push(result);
+        }
+    }
+    return opperands.top();
+}
+
+int main() {
+	string expression;
+	cout << "enter expression(infix)(single digit numbers only): ";
+	cin >> expression;
+
+	cout << "result: " << evaluteExpression(expression) << endl;
+
+}
