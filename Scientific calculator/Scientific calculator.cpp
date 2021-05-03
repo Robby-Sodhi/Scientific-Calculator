8feb5714bafeb6ba1846e8816d6e795d3a110fc9
Robby Sodhi <42954808+Robstersgaming@users.noreply.github.com>
2021-05-02 20:54:41 -0400

    multi digit support

--- Scientific calculator/Scientific calculator.cpp
+++ Scientific calculator/Scientific calculator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
 
 
 using namespace std;
@@ -43,13 +44,20 @@ bool isOperand(char input)
     return false;
 }
 
-string infixToPostfix(string input) {
+vector<string> infixToPostfix(string input) {
     stack<char> opstack;
-    string output;
+    vector<string> output;
 
     for (int i{ 0 }; i < input.size(); i++) {
         if (isOperand(input[i])) {
-            output += input[i];
+            string digit = string(1, input[i]);
+            i++;
+            while (isOperand(input[i])) {
+                digit += input[i];
+                ++i;
+            }
+            output.push_back(digit);
+            i--;
         }
         else if (input[i] == '(') {
             opstack.push(input[i]);
@@ -58,14 +66,14 @@ string infixToPostfix(string input) {
             char popped{ opstack.top() };
             opstack.pop();
             while (popped != '(') {
-                output += popped;
+                output.push_back(string(1, popped));
                 popped = opstack.top();
                 opstack.pop();
             }
         }
         else if (isOperator(input[i])) {
             while (!opstack.empty() && Prec(opstack.top()) >= Prec(input[i])) {
-                output += opstack.top();
+                output.push_back(string(1, opstack.top()));
                 opstack.pop();
             }
             opstack.push(input[i]);
@@ -73,34 +81,35 @@ string infixToPostfix(string input) {
     }
 
     while (!opstack.empty()) {
-        output += opstack.top();
+        output.push_back(string(1, opstack.top()));
         opstack.pop();
     }
 
     return output;
 }
-double performMathOperation(char opp, double first, double second) {
-    switch (opp) {
-    case '+' :
+double performMathOperation(string opp, double first, double second) {
+    if (opp.compare("+") == 0)
         return first + second;
-    case '-':
+    if (opp.compare("-") == 0)
         return first - second;
-    case '*':
+    if (opp.compare("*") == 0)
         return first * second;
-    case '/':
+    if (opp.compare("/") == 0)
         return first / second;
-    }
+
 }
 double evaluteExpression(string expression) {
-    string postfix = infixToPostfix(expression);
-    cout << postfix << endl;
+    vector<string> postfix = infixToPostfix(expression);
 
+    //postfix to infix with evaluation
     stack<double> opperands;
     for (int i = 0; i < postfix.size(); i++) {
-        if (isdigit(postfix[i])) {
-            opperands.push(stod(string(1, postfix[i])));
+
+        try {
+            double integer = stod(postfix[i]);
+            opperands.push(integer);
         }
-        else if (isOperator(postfix[i])) {
+        catch (std::invalid_argument& e) {
             double second = opperands.top();
             opperands.pop();
             double first = opperands.top();
@@ -108,15 +117,18 @@ double evaluteExpression(string expression) {
             double result = performMathOperation(postfix[i], first, second);
             opperands.push(result);
         }
+
     }
-   return opperands.top();
+
+    return opperands.top();
 }
 
 int main() {
-	string expression;
-	cout << "enter expression(infix)(single digit numbers only): ";
+
+    string expression;
+    cout << "enter expression(infix): ";
     getline(cin, expression);
 
-	cout << "result: " << evaluteExpression(expression) << endl;
+    cout << "result: " << evaluteExpression(expression) << endl;
 
 }
